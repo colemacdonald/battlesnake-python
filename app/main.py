e2d3aa92ba39074de9bfd589141a117628e67feb
ajhorsfi <ajhorsfi@gmail.com>
2017-03-04 15:43:42 -0800

    added dead end function as well as put adjacent and viable in functins

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': color,
         'taunt': taunt,
         'head_url': head_url,
         'name': my_name
@@ -58,29 +58,12 @@ def move():
     #Head coordinates and coordinates of adjacent spaces
     my_head = my_snake['coords'][0]
     
-    adjacent = {}
-    adjacent['up'] = [my_head[0], my_head[1]-1]
-    adjacent['down'] = [my_head[0], my_head[1]+1]
-    adjacent['left'] = [my_head[0]-1, my_head[1]]
-    adjacent['right'] = [my_head[0]+1, my_head[1]]
+    adjacent = getAdjacent(my_head)
+    
     
     #Determine which directions are clear to move
-    viable_move = {}
-    for direction, coord in adjacent.items():
-    	    viable_flag = True
-    	    if coord[0] < 0 or coord[0] > board_width - 1: #if X coord is a wall value don't include direction
-    	    	    viable_flag = False
-	    elif coord[1] < 0 or coord[1] > board_height - 1: #if Y coord is a wall value don't include direction
-		    viable_flag = False
-	    for snake in snakes:
-		    if viable_flag == False: #if coord == a point in previous snake then break
-			    break
-		    for point in snake['coords']: #compare coord to all body points of snake
-			    if point == coord: #if this point in snake == coord then don't include direction and break
-				    viable_flag = False
-				    break
-	    if viable_flag == True: #if viable flag is still true then add direction to possible moves	    	    	    	    	    	    
-    	    	    viable_move[direction] = coord
+    viable_move = getViable(adjacent)
+    
     if health < 50: #go get apple
         if len(food) > 0:
             distance = []
@@ -91,21 +74,57 @@ def move():
             		target = food[i]
             	elif dictance[i] < distance[i-1]:
             		target = food[i]
-            for dist in distance:
-                if target > dist:
-                    target = dist
+            
         
     else: #keep doing other stuff #TODO: Implement Snake behavioural AI
-        
+        for direction, coord in viable_move.items():
+        	if deadEnd(coord, my_head, count = 0):
+        		viable_move.pop(direction, none)
+        	
         #pick and send move
-        directions = {'up': 'up', 'down':'down', 'left':'left', 'right':'right'}
         move = viable_move.keys()[0]
     return {
         'move': move,
         'taunt': taunt
     }
-
-
+def getAdjacent(point):
+    adjacent = {}
+    adjacent['up'] = [point[0], point[1]-1]
+    adjacent['down'] = [point[0], point[1]+1]
+    adjacent['left'] = [point[0]-1, point[1]]
+    adjacent['right'] = [point[0]+1, point[1]]
+    return adjacent
+def getViable(adjacent)
+    for direction, coord in adjacent.items():
+        viable_flag = True
+        if coord[0] < 0 or coord[0] > board_width - 1: #if X coord is a wall value don't include direction
+	    viable_flag = False
+        elif coord[1] < 0 or coord[1] > board_height - 1: #if Y coord is a wall value don't include direction
+	    viable_flag = False
+        for snake in snakes:
+	    if viable_flag == False: #if coord == a point in previous snake then break
+	        break
+	    for point in snake['coords']: #compare coord to all body points of snake
+		if point == coord: #if this point in snake == coord then don't include direction and break
+		    viable_flag = False
+		    break
+        if viable_flag == True: #if viable flag is still true then add direction to possible moves	    	    	    	    	    	    
+	    viable_move[direction] = coord
+    return viable_move
+def deadEnd(currCoord, lastCoord, count)
+    if count == 5: return False
+    adjacent = getAdjacent(currCoord)
+    viable = getViable(adjacent)
+    for direction, coord in viable.items():
+    	    if coord == lastCoord:
+    	    	    viable.pop(direction, none)
+    if not viable:
+    	    return True
+    else: 
+    	    for direction, coord in viable.items()
+    	    	if !deadEnd(coord, currCoord, count):
+    	    		return False
+    	    return True
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
