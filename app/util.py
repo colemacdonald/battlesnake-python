357369b24a3c9d69a5fdd9be535862c0d15aa58d
Noah Silvera <noah.a.silvera@gmail.com>
2019-03-02 13:13:31 -0800

    add quadrant density function

--- app/util.py
+++ app/util.py
@@ -2,6 +2,17 @@ import random
 
 # contains some utility functions for getting information about the board / potential moves
 
+
+def get_quadrant_size(board):
+    if board.width < 10:
+        return 2
+    if board.width < 15:
+        return 3
+    if board.width < 20:
+        return 4
+    else:
+        return 5
+
 def is_same_space(space1, space2):
     return (space1['x'] == space2['x'] and space1['y'] == space2['y'])
 
@@ -58,7 +69,7 @@ def is_wall(move, data):
     cur_head = me['body'][0]
     new_head = convert_move_to_new_head(cur_head, move)
 
-    if new_head['x'] == -1 or new_head['x'] == width or new_head['y'] == -1 or new_head['y'] == height:
+    if new_head['x'] <= -1 or new_head['x'] >= width or new_head['y'] <= -1 or new_head['y'] => height:
         return True
     
     return False
@@ -74,3 +85,51 @@ def find_safe_move(data):
         directions.remove(d)
     
     return 'up'
+
+def get_adjacent_quadrant_densities(starting_point, data):
+    board = data['board']
+    # returns the adjacent quadrants
+    # if a wall if in a quadrant, it is not returned as a viable quadrant
+    quad_size = get_quadrant_size(board)
+    # define a quadrant by it's top left and bottom right corners
+    top_left_quad = { 
+        top_left: { 'x': starting_point['x'] - quad_size, 'y': starting_point['y'] - quad_size },
+        bottom_right: { starting_point }
+    }
+    top_right_quad = {
+        top_left: { 'x': starting_point['x'], 'y': starting_point['y'] - quad_size },
+        bottom_right: { 'x': starting_point[x] + quad_size, starting_point['y'] }
+    }
+    bottom_left_quad = { 
+        top_left: { 'x': starting_point['x'] - quad_size, 'y': starting_point['y'] },
+        bottom_right: { 'x': starting_point['x'], starting_point['y'] + quad_size }
+    }
+    bottom_right_quad = { 
+        top_left: { 'x': starting_point['x'], 'y': starting_point['y'] },
+        bottom_right: { 'x': starting_point['x'] + quad_size, 'y': starting_point['y'] + quad_size}
+    }
+
+    quad_densities = {
+        "top_left": get_quadrant_density(top_left, quad_size, data),
+        "top_right": get_quadrant_density(top_right, quad_size, data),
+        "bottom_left": get_quadrant_density(bottom_left, quad_size, data),
+        "bottom_right": get_quadrant_density(bottom_right, quad_size, data),
+    }
+
+    return quad_densities
+    
+
+def get_quadrant_density(quadrant, quad_size, data):
+    num_quad_squares = quad_size * quad_size
+    full_squares = 0
+    top_left = quadrant['top_left']
+    bottom_right = quadrant['bottom_right']
+    for x in range(top_left['x'], bottom_right['x']):
+        for y in range(top_left['y'], bottom_right['y']):
+            point = {'x': x,'y': y}
+            if is_snake(point, data) or is_wall(point, data):
+                full_squares += 1
+    return full_squares / num_quad_squares
+                
+
+
